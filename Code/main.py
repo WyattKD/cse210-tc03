1e733b0146d8c6382610af1a56acd8fccafb9ae2
WyattKD <85959188+WyattKD@users.noreply.github.com>
2021-10-02 21:42:41 -0600

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,9 @@
 from wyatt import wyatt_greeting
+from courtney import courtney_greeting
+from ethan import ethan_greeting
+
 
 print("Welcome to our collaborative program")
 wyatt_greeting()
+ethan_greeting()
+courtney_greeting()
